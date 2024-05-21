05856b41b003852f27b07d5cbab7c02d25a2e5e7
yukimasa Nomoto <yukimasa_nomoto@csc1980.co.jp>
2024-05-21 10:42:57 +0900

    Button1追加機能実装済み

--- GitHubFlowWindowApp/Form1.cs
+++ GitHubFlowWindowApp/Form1.cs
@@ -6,5 +6,10 @@ namespace GitHubFlowWindowApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button1");
+        }
     }
 }
